938e89258b99e06eedeaec63b6a8f839c1e4535b
davidjosephind <davidjosephind@gmail.com>
2022-04-20 22:52:31 +0530

    Add build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/data-structure-visualizer",
+  assetPrefix: "/data-structure-visualizer",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
