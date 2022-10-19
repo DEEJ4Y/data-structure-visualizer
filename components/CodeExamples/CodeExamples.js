ab15e9452e4d84683912a19642e5fdd1f3c1aa67
David Joseph <70807315+DEEJ4Y@users.noreply.github.com>
2022-10-19 17:20:32 +0530

    Fix: Migrating to 5.5.6 Prism tabs

--- components/CodeExamples/CodeExamples.js
+++ components/CodeExamples/CodeExamples.js
@@ -2,19 +2,23 @@ import { Prism } from "@mantine/prism";
 
 export default function CodeExamples({ operation, languages }) {
   return (
-    <Prism.Tabs>
-      {languages?.map(({ langLabel, langKey, operations }, idx) => {
-        console.log(idx);
-        return (
-          <Prism.Tab
-            key={`${langLabel}-${idx}`}
-            label={langLabel}
-            language={langKey}
-          >
-            {operations[operation]}
+    <Prism.Tabs defaultValue={languages[0].langKey}>
+      <Prism.TabsList>
+        {languages.map(({ langLabel, langKey }, idx) => (
+          <Prism.Tab key={`tab-${langLabel}-${idx}`} value={langKey}>
+            {langLabel}
           </Prism.Tab>
-        );
-      })}
+        ))}
+      </Prism.TabsList>
+      {languages.map(({ langLabel, langKey, operations }, idx) => (
+        <Prism.Panel
+          key={`panel-${langLabel}-${idx}`}
+          language={langKey}
+          value={langKey}
+        >
+          {operations[operation]}
+        </Prism.Panel>
+      ))}
     </Prism.Tabs>
   );
 }
